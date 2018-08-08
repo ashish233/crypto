b4c6d72abbd805ec513a717c7942fb267c693c17
ashish233 <pandeashish233@gmail.com>
2018-08-08 20:35:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// test 
 var app = express();
-// test for 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
