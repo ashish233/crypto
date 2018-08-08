605513d0474ccd2b475104a0ab08f84412ebc383
ashish233 <pandeashish233@gmail.com>
2018-08-08 20:32:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+// test for 
 // view engine setup
-
-// test
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
